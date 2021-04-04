ce0c8a1df278406efbd5bab45b4cf857a68f8959
coffmanjrp <coffmanjrp@gmail.com>
2021-04-03 17:33:04 -0700

    Run simple query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+let links = [
+  {
+    id: 'link-0',
+    description: 'Fullstack tutorial for GraphQL',
+    url: 'www.howtographql.com',
+  },
+];
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hacker News Clone`,
+    info: () => `This is the API of a Hacker News Clone `,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
